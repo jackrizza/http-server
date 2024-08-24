a7d7fc07f42ac130d81346bd07f053dec5ebdcbc
Augustus Rizza <jackrizza@gmail.com>
2024-08-24 18:56:45 -0400

    added ssl support

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,7 @@
 extern crate httpserver;
 use clap::Parser;
 use httpserver::datastore::{DataStore, Op};
-use httpserver::router;
+use httpserver::{http_router, https_router};
 
 /// Simple HTTP server
 /// refer to https://github.com/jackrizza/http-server
@@ -20,12 +20,27 @@ struct Args {
     /// set password for authentication (required for authenticate flag)
     #[arg(short, long, default_value_t = String::from(""))]
     password: String,
+
+    /// openssl
+    /// needed for authentication (Cookies won't work without it)
+    #[arg(short, long, default_value_t = false)]
+    openssl: bool,
+
+    #[arg(long, default_value_t = String::from(""))]
+    pem_password : String,
+
+    #[arg(long, default_value_t = String::from(""))]
+    pem_file : String,
+
+    #[arg(long, default_value_t = String::from(""))]
+    cert_file : String
+
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let args = Args::parse();
-
+    println!("{:?}", args);
     let mut datastore = DataStore::new();
     datastore.listen();
 
@@ -40,6 +55,9 @@ async fn main() -> std::io::Result<()> {
         }
         false => (),
     }
-
-    router(args.port, &mut datastore).await
+    if args.openssl {
+        https_router(args.port, &mut datastore, args.pem_file, args.pem_password, args.cert_file).await
+    } else {
+        http_router(args.port, &mut datastore).await
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,11 @@ use futures_util::future::FutureExt;
 use actix_session::config::{BrowserSession, CookieContentSecurity};
 use actix_web;
 
+use openssl::{
+    pkey::{PKey, Private},
+    ssl::{SslAcceptor, SslMethod},
+};
+
 use routes::landing::{landing, login, post_login};
 use routes::upload::{get_upload_file, post_upload_file};
 use routes::{normalize_css, skeleton_css};
@@ -19,14 +24,20 @@ pub mod auth;
 pub mod datastore;
 pub mod routes;
 
+use std::{
+    fs::File,
+    io::{self, Read as _, BufReader},
+};
+
+
 fn session_middleware() -> SessionMiddleware<CookieSessionStore> {
     SessionMiddleware::builder(CookieSessionStore::default(), Key::generate())
         .cookie_name(String::from("simplehttpkey")) // arbitrary name
-        // .cookie_secure(true) // https only
-        // .session_lifecycle(BrowserSession::default()) // expire at end of session
-        // .cookie_same_site(SameSite::Strict)
-        // .cookie_content_security(CookieContentSecurity::Private) // encrypt
-        // .cookie_http_only(true) // disallow scripts from reading
+        .cookie_secure(true) // https only
+        .session_lifecycle(BrowserSession::default()) // expire at end of session
+        .cookie_same_site(SameSite::Strict)
+        .cookie_content_security(CookieContentSecurity::Private) // encrypt
+        .cookie_http_only(true) // disallow scripts from reading
         .build()
 }
 
@@ -42,7 +53,17 @@ pub async fn auth_chain(key: String, ds: &mut DataStore) -> bool {
     }
 }
 
-pub async fn router(port: u16, ds: &mut DataStore) -> std::io::Result<()> {
+fn load_encrypted_private_key(pem_file : String, pem_password : String) -> PKey<Private> {
+    let mut file = File::open(&pem_file).unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).expect("Failed to read file");
+
+    PKey::private_key_from_pem_passphrase(&buffer, pem_password.as_bytes()).unwrap()
+}
+
+
+
+pub async fn http_router(port: u16, ds: &mut DataStore) -> std::io::Result<()> {
     let mut ds = web::Data::new(ds.clone());
 
     // configuration file or environment variables.
@@ -63,6 +84,58 @@ pub async fn router(port: u16, ds: &mut DataStore) -> std::io::Result<()> {
             .service(post_login)
     })
     .bind(("0.0.0.0", port))?
+    .run()
+    .await
+}
+
+pub async fn https_router(port: u16, ds: &mut DataStore, pem_file : String, pem_pass : String, cert_file : String) -> std::io::Result<()> {
+    let mut ds = web::Data::new(ds.clone());
+    // build TLS config from files
+    rustls::crypto::aws_lc_rs::default_provider()
+    .install_default()
+    .unwrap();
+
+    let mut certs_file = BufReader::new(File::open(&cert_file).unwrap());
+    let mut key_file = BufReader::new(File::open(&pem_file).unwrap());
+
+    // load TLS certs and key
+    // to create a self-signed temporary cert for testing:
+    // `openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost'`
+    let tls_certs = rustls_pemfile::certs(&mut certs_file)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+    let tls_key = rustls_pemfile::pkcs8_private_keys(&mut key_file)
+        .next()
+        .unwrap()
+        .unwrap();
+
+    // set up TLS config options
+    let tls_config = rustls::ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(tls_certs, rustls::pki_types::PrivateKeyDer::Pkcs8(tls_key))
+        .unwrap();
+
+    // configuration file or environment variables.
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    
+    // log::info!("starting HTTPS server at http://0.0.0.0:8443");
+    HttpServer::new(move || {
+        App::new()
+            .app_data(ds.clone())
+            .wrap(session_middleware())
+            .wrap(Logger::default())
+            .service(fs::Files::new("/static", ".").show_files_listing())
+            .service(normalize_css)
+            .service(skeleton_css)
+            .service(get_upload_file)
+            // .route("/upload_file", web::get().to( || get_upload_file(&mut ds)))
+            .service(post_upload_file)
+            .service(landing)
+            .service(login)
+            .service(post_login)
+    })
+    .bind_rustls_0_23(("0.0.0.0", 8443), tls_config)?
+    .workers(2)
     .run()
     .await
 }
